e269d5340023a64a0008c37a0effc851d63b4b32
Luis Felipe Mileo <mileo@kmee.com.br>
2016-10-09 23:38:24 -0300

    [REF] Change dependencies files according OCA maintainer quality tools

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,11 @@
-sudo apt-get install nano python-virtualenv gcc python2.7-dev libxml2-dev libxslt1-dev libevent-dev libsasl2-dev libldap2-dev libpq-dev libpng12-dev libjpeg-dev node-less node-clean-css xfonts-75dpi xfonts-base
+pip install -q flake8 Click pylint-mccabe
+# Install pylint plugin depends without lxml
+wget -q https://raw.githubusercontent.com/OCA/pylint-odoo/master/requirements.txt -O ${HOME}/maintainer-quality-tools/travis/pylint_odoo_requirements.txt
+find -L ${HOME}/maintainer-quality-tools/travis -name pylint_odoo_requirements.txt -exec sed -i '/lxml/d'  {} \;  #  lxml depends is too slow
+pip install --upgrade -r ${HOME}/maintainer-quality-tools/travis/pylint_odoo_requirements.txt
+pip install --upgrade --pre --no-deps git+https://github.com/OCA/pylint-odoo.git   # To use last version ever
+npm install -g jshint  # Extra package for pylint-odoo plugin
+
+ln -s `which nodejs` $HOME/maintainer-quality-tools/travis/node
+npm install -g less less-plugin-clean-css
 
