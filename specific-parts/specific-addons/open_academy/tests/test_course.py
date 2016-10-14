a4cb8c81d1525e628b4dd6c5771c0e4f9ddb1859
Luis Felipe Mileo <mileo@kmee.com.br>
2016-10-14 16:57:44 -0300

    [REM] Removing some tests

--- specific-parts/specific-addons/open_academy/tests/test_course.py
+++ specific-parts/specific-addons/open_academy/tests/test_course.py
@@ -30,129 +30,129 @@ class TestOpenAcademy(common.TransactionCase):
         self.assertEquals(len(courses), 3,
             "Os cursos inseridos via xml de dados não estão corretos")
 
-    def test_exercise_08(self):
-        sesison_model = self.env['openacademy.session']
-        python_session = sesison_model.create({
-            'name': 'Python 2016/2',
-            'start_date': datetime.datetime.now(),
-            'duration': 2,
-            'seats': 10,
-        })
-
-    def test_exercise_09(self):
-        course_model = self.env['openacademy.course']
-        self.res_partner_model = self.env['res.partner']
-        self.partner = self.browse_ref('base.res_partner_2')
-        elm_course = course_model.create({
-            'name': 'ELM Course',
-            'description': 'Chega de dor de cabeça no front end',
-            'responsible_id': self.partner.id,
-        })
-        self.res_partner_model = self.env['res.partner']
-        self.elm_instructor = self.browse_ref('base.res_partner_3')
-        sesison_model = self.env['openacademy.session']
-        elm_session = sesison_model.create({
-            'name': 'ELM 2016/2',
-            'start_date': datetime.datetime.now(),
-            'duration': 2,
-            'seats': 10,
-            'instructor_id': self.elm_instructor.id,
-            'course_id': elm_course.id,
-        })
-
-    def test_exercise_10(self):
-        course_model = self.env['openacademy.course']
-        self.res_partner_model = self.env['res.partner']
-        self.partner = self.browse_ref('base.res_partner_2')
-        elm_course = course_model.create({
-            'name': 'ELM Course',
-            'description': 'Chega de dor de cabeça no front end',
-            'responsible_id': self.partner.id,
-        })
-        self.res_partner_model = self.env['res.partner']
-        self.elm_instructor = self.browse_ref('base.res_partner_3')
-        sesison_model = self.env['openacademy.session']
-        elm_session_1 = sesison_model.create({
-            'name': 'ELM 2016/1',
-            'start_date': datetime.datetime.now(),
-            'duration': 2,
-            'seats': 10,
-            'instructor_id': self.elm_instructor.id,
-            'course_id': elm_course.id,
-        })
-        elm_session_2 = sesison_model.create({
-            'name': 'ELM 2016/2',
-            'start_date': datetime.datetime.now(),
-            'duration': 2,
-            'seats': 10,
-            'instructor_id': self.elm_instructor.id,
-            'course_id': elm_course.id,
-        })
-        self.assertEquals(len(elm_course.session_ids), 2,
-            "A quantidade de sessões do curso de EML esta incorreta")
-
-    def test_exercise_11(self):
-        sesison_model = self.env['openacademy.session']
-        attendees = self.browse_ref(
-            'base.res_partner_2') | self.browse_ref('base.res_partner_3')
-        python_session = sesison_model.create({
-            'name': 'Python 2016/2',
-            'start_date': datetime.datetime.now(),
-            'duration': 2,
-            'seats': 10,
-            'attendee_ids': (6, 0, [attendees])
-        })
-        # TODO
-
-    def test_exercise_12(self):
-        partner = self.browse_ref('base.res_partner_2')
-        partner.write(
-            {'instructor': True}
-        )
-
-    def test_exercise_15(self):
-        # taken_seats
-        self.assertTrue(False, " ")
-
-    def test_exercise_16(self):
-        # default values
-        self.assertTrue(False, " ")
-
-    def test_exercise_17(self):
-        # on change
-        self.assertTrue(False, " ")
-
-    def test_exercise_18(self):
-        # constraint python
-        self.assertTrue(False, " ")
-
-    def test_exercise_20(self):
-        # constraint SQL
-        self.assertTrue(False, " ")
-
-    def test_exercise_21(self):
-        # duplicate
-        self.assertTrue(False, " ")
-
-    def test_exercise_28(self):
-        # workflow
-        self.assertTrue(False, " ")
-
-    def test_exercise_30(self):
-        # automated transitions
-        self.assertTrue(False, " ")
-
-    def test_exercise_31(self):
-        # Server action
-        self.assertTrue(False, " ")
-
-    def test_exercise_32(self):
-        # Access control
-        self.assertTrue(False, " ")
-
-    def test_exercise_35(self):
-        # Wizard
-        self.assertTrue(False, " ")
+    # def test_exercise_08(self):
+    #     sesison_model = self.env['openacademy.session']
+    #     python_session = sesison_model.create({
+    #         'name': 'Python 2016/2',
+    #         'start_date': datetime.datetime.now(),
+    #         'duration': 2,
+    #         'seats': 10,
+    #     })
+    #
+    # def test_exercise_09(self):
+    #     course_model = self.env['openacademy.course']
+    #     self.res_partner_model = self.env['res.partner']
+    #     self.partner = self.browse_ref('base.res_partner_2')
+    #     elm_course = course_model.create({
+    #         'name': 'ELM Course',
+    #         'description': 'Chega de dor de cabeça no front end',
+    #         'responsible_id': self.partner.id,
+    #     })
+    #     self.res_partner_model = self.env['res.partner']
+    #     self.elm_instructor = self.browse_ref('base.res_partner_3')
+    #     sesison_model = self.env['openacademy.session']
+    #     elm_session = sesison_model.create({
+    #         'name': 'ELM 2016/2',
+    #         'start_date': datetime.datetime.now(),
+    #         'duration': 2,
+    #         'seats': 10,
+    #         'instructor_id': self.elm_instructor.id,
+    #         'course_id': elm_course.id,
+    #     })
+    #
+    # def test_exercise_10(self):
+    #     course_model = self.env['openacademy.course']
+    #     self.res_partner_model = self.env['res.partner']
+    #     self.partner = self.browse_ref('base.res_partner_2')
+    #     elm_course = course_model.create({
+    #         'name': 'ELM Course',
+    #         'description': 'Chega de dor de cabeça no front end',
+    #         'responsible_id': self.partner.id,
+    #     })
+    #     self.res_partner_model = self.env['res.partner']
+    #     self.elm_instructor = self.browse_ref('base.res_partner_3')
+    #     sesison_model = self.env['openacademy.session']
+    #     elm_session_1 = sesison_model.create({
+    #         'name': 'ELM 2016/1',
+    #         'start_date': datetime.datetime.now(),
+    #         'duration': 2,
+    #         'seats': 10,
+    #         'instructor_id': self.elm_instructor.id,
+    #         'course_id': elm_course.id,
+    #     })
+    #     elm_session_2 = sesison_model.create({
+    #         'name': 'ELM 2016/2',
+    #         'start_date': datetime.datetime.now(),
+    #         'duration': 2,
+    #         'seats': 10,
+    #         'instructor_id': self.elm_instructor.id,
+    #         'course_id': elm_course.id,
+    #     })
+    #     self.assertEquals(len(elm_course.session_ids), 2,
+    #         "A quantidade de sessões do curso de EML esta incorreta")
+    #
+    # def test_exercise_11(self):
+    #     sesison_model = self.env['openacademy.session']
+    #     attendees = self.browse_ref(
+    #         'base.res_partner_2') | self.browse_ref('base.res_partner_3')
+    #     python_session = sesison_model.create({
+    #         'name': 'Python 2016/2',
+    #         'start_date': datetime.datetime.now(),
+    #         'duration': 2,
+    #         'seats': 10,
+    #         'attendee_ids': (6, 0, [attendees])
+    #     })
+    #     # TODO
+    #
+    # def test_exercise_12(self):
+    #     partner = self.browse_ref('base.res_partner_2')
+    #     partner.write(
+    #         {'instructor': True}
+    #     )
+    #
+    # def test_exercise_15(self):
+    #     # taken_seats
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_16(self):
+    #     # default values
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_17(self):
+    #     # on change
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_18(self):
+    #     # constraint python
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_20(self):
+    #     # constraint SQL
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_21(self):
+    #     # duplicate
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_28(self):
+    #     # workflow
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_30(self):
+    #     # automated transitions
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_31(self):
+    #     # Server action
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_32(self):
+    #     # Access control
+    #     self.assertTrue(False, " ")
+    #
+    # def test_exercise_35(self):
+    #     # Wizard
+    #     self.assertTrue(False, " ")
 
 
 
